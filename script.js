b49a9a4c6ca678405f36e2d7679c8b48b207ced8
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-05-18 21:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+const btn = document.getElementById('btn');
+const op = document.getElementById('output');
+
+async function fetchData(url1){
+	try {
+		const response = await fetch(url1);
+		let data = await response.json();
+		// console.log(data);
+		return data;
+		
+	} catch (error) {
+		return {error:'failed to fetch data'}
+	}
+}
+
+btn.addEventListener('click',async ()=>{
+	const result = await fetchData(url);
+	op.textContent = JSON.stringify(result);
+})
